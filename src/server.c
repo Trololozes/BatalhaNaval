6a308e63069aa597c99f6aa8810647054bfd4623
caedus75 <carlos4735@gmail.com>
2015-05-16 04:59:25 -0300

    server encerra graciosamente com SIGINT
    'ctrl+C' faz server deixar de aceitar conexoes, envia uma mensagem
    a todos conectados, derruba eles, limpa tudo e encerra.

--- src/server.c
+++ src/server.c
@@ -23,6 +23,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -32,19 +33,34 @@
 
 #define PORT 5824
 #define PENDING 5
+#define MAX_CONN 50
 
+void sighandler(int);
+void *stop_listening(void*);
 void *connect_client(void*);
 
 static volatile int connect_c = 0;
-static pthread_mutex_t lock;
+static bool run_Forrest_run = true;
+static pthread_mutex_t counter_lock;
+static pthread_mutex_t sock_kill_lock;
+static pthread_cond_t sock_kill_cond;
 
 int main(int argc, char *argv[]){
     int list_sock;
-    int *conn_sock;
+    int *conn_sock[MAX_CONN] = { NULL };
+    int **conn_ptr;
     struct sockaddr_in serv_addr;
     struct sockaddr_in cli_addr;
     socklen_t sock_len;
     pthread_t thread;
+
+    pthread_cond_init(&sock_kill_cond, NULL);
+    pthread_mutex_init(&sock_kill_lock, NULL);
+    pthread_mutex_init(&counter_lock, NULL);
+
+    conn_ptr = conn_sock;
+
+    signal(SIGINT, sighandler);
 
     if( (list_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0 ){
         perror("socket() error\n");
@@ -62,28 +78,59 @@ int main(int argc, char *argv[]){
     }
 
     listen(list_sock, PENDING);
+    pthread_create(&thread, NULL, stop_listening, (void *)&list_sock);
 
-    pthread_mutex_init(&lock, NULL);
-
-    while( true ){
-        conn_sock = malloc(sizeof *conn_sock);
+    while( run_Forrest_run ){
+        *conn_ptr = malloc(sizeof **conn_ptr);
         sock_len = sizeof ( struct sockaddr_in );
 
-        *conn_sock = accept(list_sock, (struct sockaddr *)&cli_addr, &sock_len);
-        if( *conn_sock < 0 ){
+        **conn_ptr = accept(list_sock, (struct sockaddr *)&cli_addr, &sock_len);
+        if( **conn_ptr < 0 ){
             perror("accept() error\n");
             perror(strerror(errno));
+            continue;
         }
 
-        if( pthread_create(&thread, NULL, connect_client, (void *)conn_sock) ){
-            close(list_sock);
-            close(*conn_sock);
+        if( pthread_create(&thread, NULL, connect_client, (void *)*conn_ptr) ){
+            close(**conn_ptr);
+            free(*conn_ptr);
+            conn_ptr--;
+        }
+
+        conn_ptr++;
+    }
+
+    for( int i = 0; i < MAX_CONN; i++ ){
+        if( conn_sock[i] != NULL ){
+            write(*conn_sock[i], "Server down\n", 12);
+            free(conn_sock[i]);
+            conn_sock[i] = NULL;
         }
     }
 
-    pthread_mutex_destroy(&lock);
+    pthread_mutex_destroy(&counter_lock);
+    pthread_mutex_destroy(&sock_kill_lock);
+    pthread_cond_destroy(&sock_kill_cond);
 
     exit(EXIT_SUCCESS);
+}
+
+void sighandler(int sig){
+    pthread_mutex_lock(&sock_kill_lock);
+    pthread_cond_broadcast(&sock_kill_cond);
+
+    run_Forrest_run = false;
+
+    pthread_mutex_unlock(&sock_kill_lock);
+}
+
+void *stop_listening(void *sock){
+    pthread_mutex_lock(&sock_kill_lock);
+    pthread_cond_wait(&sock_kill_cond, &sock_kill_lock);
+
+    shutdown(*(int *)sock, SHUT_RD);
+
+    return 0;
 }
 
 void *connect_client(void *sock){
@@ -94,12 +141,10 @@ void *connect_client(void *sock){
     char msg_in[size];
     char msg_out[size];
 
-    free(sock);
-
-    pthread_mutex_lock(&lock);
+    pthread_mutex_lock(&counter_lock);
     id = connect_c + 1;
     connect_c++;
-    pthread_mutex_unlock(&lock);
+    pthread_mutex_unlock(&counter_lock);
 
     sprintf(msg_out, "Hello, you are #%d!!\n", id);
     write(l_sock, msg_out, strlen(msg_out));
@@ -113,10 +158,10 @@ void *connect_client(void *sock){
         memset(msg_in, 0, size);
     }
 
-    pthread_mutex_lock(&lock);
+    pthread_mutex_lock(&counter_lock);
     fprintf(stdout, "Connection #%d closed\n", id);
     connect_c--;
-    pthread_mutex_unlock(&lock);
+    pthread_mutex_unlock(&counter_lock);
 
     return 0;
 }
